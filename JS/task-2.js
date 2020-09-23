204d2da59d59d6a08ede2bd298dc8870bd62786c
Zoriana-Burian <ke7sha1991@gmail.com>
2020-09-23 13:10:22 +0300

    Рішення задачки 2

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const ingredients = [
     'Картошка',
     'Грибы',
@@ -7,4 +9,14 @@ const ingredients = [
     'Приправы',
   ];
 
- 
+  const ulList = document.getElementById("ingredients");
+  const elements = ingredients.map(ingredient => {
+    const items = document.createElement('li');
+    items.innerHTML = ingredient;
+    return items;
+  })
+  console.log(elements);
+  ulList.append(...elements);
+
+
+  
