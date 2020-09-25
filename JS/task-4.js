25d56374720cf5ad82d7f250e465a2a6e88d6edd
Zoriana-Burian <ke7sha1991@gmail.com>
2020-09-25 14:18:33 +0300

    рішення 4 задачки

---
+++ JS/task-4.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const counterValue = {
+    value: 0,
+    
+    decrement() {
+        this.value -= 1;
+    },
+    
+    increment() {
+        this.value += 1;
+    },
+};
+
+const decrementBtn = document.querySelector("[data-action='decrement']");
+const incrementBtn = document.querySelector("[data-action='increment']");
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', function() {
+    counterValue.decrement();
+    valueEl.textContent = counterValue.value;
+    
+});
+
+incrementBtn.addEventListener('click', function() {
+    counterValue.increment();
+    valueEl.textContent = counterValue.value;
+});
+
