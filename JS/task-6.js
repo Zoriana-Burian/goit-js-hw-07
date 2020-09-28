adba4aead2b8557e0319ee65f2bdfefeb346c43d
Zoriana-Burian <ke7sha1991@gmail.com>
2020-09-28 13:06:44 +0300

    рішення 6 задачки

--- JS/task-6.js
+++ JS/task-6.js
@@ -1,1 +1,19 @@
 "use strict";
+
+
+const inputValid = document.querySelector('#validation-input');
+const inputLength = Number(inputValid.dataset.length);
+
+inputValid.addEventListener('blur', onInputValueCheck);
+
+function onInputValueCheck(event) {
+  if (
+    inputLength === event.currentTarget.value.length
+  ) {
+    inputValid.classList.add('valid');
+    inputValid.classList.remove('invalid');
+  } else {
+    inputValid.classList.add('invalid');
+    inputValid.classList.remove('valid');
+  }
+}
